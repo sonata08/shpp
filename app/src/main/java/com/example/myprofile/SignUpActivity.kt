f5541d81fe1fa835d35afc4d6af34106d93aa374
Nataliia Mantrova <nmantrova@gmail.com>
2023-07-24 22:17:13 +0300

    button style in progress

---
+++ app/src/main/java/com/example/myprofile/SignUpActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myprofile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myprofile.databinding.ActivitySignUpBinding
+
+class SignUpActivity: AppCompatActivity() {
+
+    lateinit var binding: ActivitySignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
