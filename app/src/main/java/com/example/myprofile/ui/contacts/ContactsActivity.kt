af80e3904e69b120523403396ddda7c1e5756e01
Nataliia Mantrova <nmantrova@gmail.com>
2023-08-08 18:11:21 +0300

    task 2 start

---
+++ app/src/main/java/com/example/myprofile/ui/contacts/ContactsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myprofile.ui.contacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myprofile.R
+
+class ContactsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contacts)
+    }
+}
