d72385eb57b133bcd830cb4de4d8215949ba1be4
Nataliia Mantrova <nmantrova@gmail.com>
2023-08-09 20:54:18 +0300

    task 2 - adapter is ready

--- app/src/main/java/com/example/myprofile/ui/contacts/ContactsActivity.kt
+++ app/src/main/java/com/example/myprofile/ui/contacts/ContactsActivity.kt
@@ -18,3 +18,4 @@ class ContactsActivity : AppCompatActivity() {
 
     }
 }
+
---
+++ app/src/main/java/com/example/myprofile/ui/contacts/ContactsAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.myprofile.ui.contacts
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myprofile.data.model.Contact
+import com.example.myprofile.databinding.ItemContactBinding
+import com.example.myprofile.utils.extentions.loadImage
+
+class ContactsAdapter() : ListAdapter<Contact, ContactsAdapter.ContactViewHolder>(ContactsDiffCallBack()) {
+    class ContactViewHolder(private val binding: ItemContactBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(contact: Contact) {
+            binding.apply {
+                profilePic.loadImage(contact.photo)
+                tvName.text = contact.name
+                tvCareer.text = contact.career
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        return ContactViewHolder(ItemContactBinding.inflate(LayoutInflater.from(parent.context)))
+    }
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.bind(current)
+    }
+
+    private class ContactsDiffCallBack : DiffUtil.ItemCallback<Contact>() {
+        override fun areItemsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+
+
+
+}
