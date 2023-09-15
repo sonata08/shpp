07ca4f332267b9c1b8236d9b931e7f1289bc4a41
Nataliia Mantrova <nmantrova@gmail.com>
2023-09-15 20:17:04 +0300

    task 4 - hilt setup

--- app/src/main/java/com/example/myprofile/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/myprofile/ui/activities/MainActivity.kt
@@ -3,11 +3,10 @@ package com.example.myprofile.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myprofile.R
+import dagger.hilt.android.AndroidEntryPoint
 
+//@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-//    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-//    private val dataStorePreferences: DataStorePreferences by lazy { DataStorePreferences(dataStore) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
